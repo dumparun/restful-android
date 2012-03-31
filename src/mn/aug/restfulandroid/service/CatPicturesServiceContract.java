17f646261df487ccde16e00c59f8b43d6f649b0a
Brad Armstrong <hashbrown1@gmail.com>
2012-03-31 14:22:08 -0500

    apply changes from presentation

---
+++ src/mn/aug/restfulandroid/service/CatPicturesServiceContract.java
@@ -0,0 +1,22 @@
+package mn.aug.restfulandroid.service;
+
+import mn.aug.restfulandroid.processor.ResourceProcessorCallback;
+import mn.aug.restfulandroid.rest.method.RestMethodFactory.Method;
+
+public interface CatPicturesServiceContract {
+
+	public static final String METHOD_EXTRA = "mn.aug.restfulandroid.service.METHOD_EXTRA";
+	public static final String RESOURCE_TYPE_EXTRA = "mn.aug.restfulandroid.service.RESOURCE_TYPE_EXTRA";
+	public static final String SERVICE_CALLBACK_EXTRA = "mn.aug.restfulandroid.service.SERVICE_CALLBACK";
+	public static final String ORIGINAL_INTENT_EXTRA = "mn.aug.restfulandroid.service.ORIGINAL_INTENT_EXTRA";
+	public static final String EXTRA_REQUEST_PARAMETERS = "mn.aug.restfulandroid.service.REQUEST_PARAMS_EXTRA";
+
+	public static final int RESOURCE_TYPE_CAT_PICTURES = 1;
+	public static final int RESOURCE_TYPE_COMMENTS = 2;
+
+	public static final String METHOD_GET = Method.GET.toString();
+	public static final String METHOD_POST = Method.POST.toString();
+
+	ResourceProcessorCallback makeCatPicturesProcessorCallback();
+
+}
